3724e6a27a001e9b0b6fa198ea251e804217730b
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2017-02-25 12:29:23 +0100

    Updated

--- senia-browsers-settings.install/etc/firefox/syspref.senia.js
+++ senia-browsers-settings.install/etc/firefox/syspref.senia.js
@@ -1,3 +1,4 @@
 // Senia settings
 lockPref("browser.startup.homepage", "https://docent.edu.gva.es/md-front/www/ | http://www.ieslasenia.es/www/");
+lockPref("browser.privatebrowsing.autostart","true");
 
